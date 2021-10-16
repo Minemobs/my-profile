04699270c084fa954bcdd3e0b15a381881754067
minemobs <issahakim94@gmail.com>
2021-10-16 14:13:32 +0200

    Configured everything

--- assets/config.js
+++ assets/config.js
@@ -1,28 +1,26 @@
 const config = {
     
-    USERNAME: "User#0000",
+    USERNAME: "minemobs#6904",
     /**
      * This can either be a file link (relative or external)
      * or a Gravatar email or hash.
      */
-    PICTURE: "assets/img/profile_picture.png",
+    PICTURE: "assets/img/minemobsPDP.png",
 
-    DESCRIPTION: "Change this bio in the <b>assets/config.js</b> file. <i>It may include HTML syntax</i>",
+    DESCRIPTION: "Java, Kotlin, TS and C# developper",
 
     BADGES: [
-        { id: "HypeSquad Bravery", image: "assets/img/hypesquad_bravery.svg" },
-        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+        { id: "HypeSquad Brilliance", image: "assets/img/HypeSquadHouseBrilliance.svg" },
     ],
 
     PROJECTS: [
-        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com", name: "Super cool project 1" },
-        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.com", name: "Super cool project 2" },
-        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com", name: "Super cool project 3" },
+        { id: "Superpack", image: "assets/img/Superpack.png", link: "https://discord.gg/tGeUTkrxhA", name: "SuperPack is a Expert-pack in 1.16.5 based on the mod Create and intelligent automation." },
+        { id: "project-2", image: "assets/img/Simplebot.png", link: "https://github.com/TheSimpleTeam/SimpleBot", name: "SimpleBot is a discord bot wrote in java using the wrapper JDA." },
     ],
 
     SOCIALS: [
-        { class_name: "fab fa-github", link: "https://github.com/discord" },
-        { class_name: "fab fa-twitter", link: "https://twitter.com/discord" },
+        { class_name: "fab fa-github", link: "https://github.com/minemobs" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/minemobs_" },
     ],
 
 }
